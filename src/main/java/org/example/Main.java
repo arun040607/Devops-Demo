d3338cd7632096dbce3d1eed3d515858ef4226a2
ARUN N <arun040607@gmail.com>
2026-09-25 09:59:00 +0530

    Update greeting message to include 'Jenkins'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,6 @@ public class Main {
 // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("Hi");
-        System.out.println("DevOps");
+        System.out.println("DevOps Jenkins");
     }
 }
